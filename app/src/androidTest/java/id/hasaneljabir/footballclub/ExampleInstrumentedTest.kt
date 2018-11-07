35d6602b316b7ddba5bf6468e0eba5403591ef00
Hasan El Jabir <eljabirhasan@gmail.com>
2018-11-07 14:37:46 +0700

    HS : Update Android Studio config

--- app/src/androidTest/java/id/hasaneljabir/footballclub/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/id/hasaneljabir/footballclub/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package id.hasaneljabir.footbalclub
+package id.hasaneljabir.footballclub
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
--- app/src/test/java/id/hasaneljabir/footballclub/ExampleUnitTest.kt
+++ app/src/test/java/id/hasaneljabir/footballclub/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package id.hasaneljabir.footbalclub
+package id.hasaneljabir.footballclub
 
 import org.junit.Test
 
